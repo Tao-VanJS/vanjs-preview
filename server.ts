f3ad11d40b3fa6fea4c9f32f242d89e5576f9e4e
Tao Xin <tao@vanjs.org>
2023-11-15 10:37:41 -0800

    Bug fix

--- server.ts
+++ server.ts
@@ -12,17 +12,20 @@ const randomKey = () => Math.random().toString(36).substring(6)
 
 Deno.serve(async req => {
   if (req.method === "POST" && GEN_PREVIEW_URL_PATTERN.test(req.url)) {
-    const data = decoder.decode(await toArrayBuffer(req.body!))
+    const data = await req.text()
     const key = randomKey()
+    // console.log({key, data})
     kv.set([key], data, {expireIn: 86400 * 1000})
     return new Response("https://vanjs-preview.deno.dev/" + key)
   }
 
   const match = PREVIEW_PATTERN.exec(req.url)
   if (match) {
-    const {value} = await kv.get<string>([match.pathname.groups.key!])
+    const key = match.pathname.groups.key!
+    const {value} = await kv.get<string>([key])
+    // console.log({key, value})
     if (!value) return new Response("Invalid or expired link", {status: 404})
-    return new Response((await kv.get<string>([match.pathname.groups.key!])).value)
+    return new Response(value)
   }
   return new Response("Not Found", {status: 404})
 })
