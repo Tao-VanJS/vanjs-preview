0eea92f61fc93044675a1d0f378828cc207157f8
Tao Xin <tao@vanjs.org>
2023-11-15 16:50:33 -0800

    Try to use jsfiddle for preview

--- server.ts
+++ server.ts
@@ -1,14 +1,24 @@
 /// <reference lib="deno.unstable" />
 
 const GEN_PREVIEW_URL_PATTERN = new URLPattern({pathname: "/gen-preview-url"})
+const JSFIDDLE_PREVIEW_PATTERN = new URLPattern({pathname: "/jsfiddle/:key"})
 const PREVIEW_PATTERN = new URLPattern({pathname: "/:key"})
 
 const kv = await Deno.openKv()
 
 const randomKey = () => Math.random().toString(36).substring(6)
 
+const escapeMap: Record<string, string> = {
+  '&': '&amp;',
+  '<': '&lt;',
+  '>': '&gt;',
+}
+
+const escapeText = (s: string) => s.replace(/[&<>]/g, tag => escapeMap[tag] || tag)
+
 const escapeAttr = (v: string) => v.replaceAll('"', "&quot;")
 
+const jsfiddleTemplate = await Deno.readTextFile("./jsfiddle-template.html")
 const template = await Deno.readTextFile("./template.html")
 
 Deno.serve(async req => {
@@ -31,7 +41,7 @@ Deno.serve(async req => {
     console.log({key, data})
     kv.set([key], data, {expireIn: 86400 * 1000})
     return new Response(
-      JSON.stringify("https://vanjs-preview.deno.dev/" + key),
+      JSON.stringify("https://vanjs-preview.deno.dev/jsfiddle/" + key),
       {
         status: 200,
         headers: {
@@ -42,20 +52,37 @@ Deno.serve(async req => {
       },
     )
   }
-
-  const match = PREVIEW_PATTERN.exec(req.url)
-  if (match) {
-    const key = match.pathname.groups.key!
-    const {value} = await kv.get<string>([key])
-    console.log({key, value})
-    if (!value) return new Response("Invalid or expired link", {status: 404})
-    const data = {
-      ...JSON.parse(value),
-      js_external: "https://cdn.jsdelivr.net/gh/vanjs-org/van/public/van-1.2.6.nomodule.min.js",
+  {
+    const match = JSFIDDLE_PREVIEW_PATTERN.exec(req.url)
+    if (match) {
+      const key = match.pathname.groups.key!
+      const {value} = await kv.get<string>([key])
+      console.log({op: "jsfiddle", key, value})
+      if (!value) return new Response("Invalid or expired link", {status: 404})
+      const json = JSON.parse(value)
+      return new Response(
+        jsfiddleTemplate
+          .replace("{{js}}", escapeText(json.js ?? ""))
+          .replace("{{css}}", escapeText(json.css ?? "")),
+        {status: 200, headers: {"Content-Type": "text/html; charset=utf-8"}},
+      )
     }
-    return new Response(
-      template.replace("{{value}}", JSON.stringify(escapeAttr(JSON.stringify(data)))),
-      {status: 200, headers: {"content-type": "text/html; charset=utf-8"}})
+  }
+  {
+    const match = PREVIEW_PATTERN.exec(req.url)
+    if (match) {
+      const key = match.pathname.groups.key!
+      const {value} = await kv.get<string>([key])
+      console.log({key, value})
+      if (!value) return new Response("Invalid or expired link", {status: 404})
+      const data = {
+        ...JSON.parse(value),
+        js_external: "https://cdn.jsdelivr.net/gh/vanjs-org/van/public/van-1.2.6.nomodule.min.js",
+      }
+      return new Response(
+        template.replace("{{value}}", JSON.stringify(escapeAttr(JSON.stringify(data)))),
+        {status: 200, headers: {"Content-Type": "text/html; charset=utf-8"}})
+    }
   }
   return new Response("Not Found", {status: 404})
 })
